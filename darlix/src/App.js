55c2ffea8a4aa825ed7099d2f1af25ed06e0eafd
Dash-02 <dasha.semivolos@gmail.com>
2024-04-12 03:01:18 +0300

    header

---
+++ darlix/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header/Header.jsx';
+
+function App() {
+  return (
+    <div>
+        {<Header />}
+        <section className='main'></section>
+      
+    </div>
+  );
+}
+
+export default App;
