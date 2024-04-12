98e45a9a8fa385a9bd1e2b1c93370096f1280f3a
Dash-02 <dasha.semivolos@gmail.com>
2024-04-12 23:20:54 +0300

    nav-themes

--- darlix/src/Header/Header.jsx
+++ darlix/src/Header/Header.jsx
@@ -1,18 +1,30 @@
 import './Header.css';
-import ImgLogo from '../assets/logo/LogoDarlix.png';
+import ImgLogo from '../assets/logo/LogoDarlix.svg';
+import ImgSun from '../assets/logo/sun.svg';
+import ImgMoon from '../assets/logo/moon.svg';
 
 function Header() {
   return (
     <header className='header'>
       <img className='logo-img' src={ImgLogo} alt="logo" />
-      <div className='logo-text'>Darlix</div>
+      <div className='logo-text'>
+        <a href="Home">Darlix</a>
+      </div>
       <div className='line'></div>
       <nav>
-        <a href="Home"></a>
-        <a href="#AboutMe"></a>
-        <a href="Works"></a>
-        <a href="Contacts"></a>
+        <a href="Home">Home</a>
+        <a href="AboutMe">About me</a>
+        <a href="Works">Works</a>
+        <a href="Contacts">Contacts</a>
       </nav>
+      <div className='themes'>
+        <button>
+          <img src={ImgSun} alt="light" />
+        </button>
+        <button>
+          <img src={ImgMoon} alt="dark" />
+        </button>
+      </div>
     </header>
   );
 }
